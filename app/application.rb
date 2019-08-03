d1ec3b4f8a9ae8eab2efe3b926d9632f6d7a5f3b
Becky Phan <beckyphan@live.com>
2019-08-03 19:07:37 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -6,13 +6,13 @@ class Application
     resp = Rack::Response.new 
     req = Rack::Request.new(env)
   
-  if req.path.match(/items/)
-    item_name = req.path.split("/items/").last
-      item = @@item.find{|i| i.name = item_name}
- 
-      resp.write item.price
-  else 
-     resp.write "Route not found"
-     resp.status = 404
-  end 
+    if req.path.match(/items/)
+      item_name = req.path.split("/items/").last
+        item = @@item.find{|i| i.name = item_name}
+   
+        resp.write item.price
+    else 
+       resp.write "Route not found"
+       resp.status = 404
+    end 
 end 
