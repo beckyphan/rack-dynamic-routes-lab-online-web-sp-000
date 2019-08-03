299a35c022a932955ad3cd743b8e3940bbc3a95f
Becky Phan <beckyphan@live.com>
2019-08-03 19:07:34 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -5,7 +5,6 @@ class Application
   def call(env)
     resp = Rack::Response.new 
     req = Rack::Request.new(env)
-  end 
   
   if req.path.match(/items/)
     item_name = req.path.split("/items/").last
